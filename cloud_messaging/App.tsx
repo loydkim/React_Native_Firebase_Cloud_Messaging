5684a45d06c0019be59cd2b2b314d7188698a9e4
Loydkim <kimloydmail@gmail.com>
2023-03-28 17:57:16 -0700

    add code

--- cloud_messaging/App.tsx
+++ cloud_messaging/App.tsx
@@ -5,9 +5,11 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect} from 'react';
 import {
+  Button,
+  PermissionsAndroid,
+  Platform,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -17,42 +19,70 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import messaging from '@react-native-firebase/messaging';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const FIREBASE_SERVER_KEY =
+  'AAAAGyacEv4:APA91bH-ihUHU9g8Bt4WF2qS5ctwff1PhHUh-yQZMcQI4Foru3l8Ef98f3JsfiEAWfzIhiBW8-BeWXvIoiLDjaR4Ac6q1une3aaeJ7jsmqGHBOdazBbQD0z6KkKOUEZJ_xrRRTwyXL6I';
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const iOSFCM =
+  'esWR9AgQykgPmC7S0KLR36:APA91bGRg9mloqR48fLGTRr0O42eCuRznVQQOi46n6AThEyDv34MiDqDcUTRut5R90jKZYGQjnvu7rKSqLwCwuupRbWSv1jctOoY-l1YMA8n0E8CSwHscBXX2i6qYlti791CiC_Sy9PP';
+const androidFCM =
+  'eKmRpM1VS6yEN4bP20xVIC:APA91bFmpDJy5h_MzDC40_HRijrk0IKN-ITasgk6C0EOGM-uwX7FmFTvxWng8Sfahla8OSjQupcEOiwrSUDtubpzvQob_YDniAhcH8aIZzMwll4zLqc6TnyF150atQuk0LWz6jwlxKOC';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
+
+function requestAndroidPermission() {
+  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+}
+
+async function requestUserPermission() {
+  const authorizationStatus = await messaging().requestPermission();
+
+  if (authorizationStatus) {
+    console.log('Permission status:', authorizationStatus);
+  }
+}
+// Reference link: https://www.inkoop.io/blog/how-to-send-push-notifications-to-specific-devices-in-react-native-using-firebase/
+// https://firebase.google.com/docs/cloud-messaging/http-server-ref
+async function sendMessage(token: string) {
+  const message = {
+    registration_ids: [token],
+    notification: {
+      title: 'Hello',
+      body:
+        Platform.OS == 'ios'
+          ? 'Send a message from iOS'
+          : 'Send a message from Android',
+      vibrate: 1,
+      sound: 1,
+      image:
+        'https://yt3.googleusercontent.com/ytc/AL5GRJVhQ4VfaYk7tLNMPDyNkgjTqWKnOXhA-NQZ1FFDUA=s176-c-k-c0x00ffffff-no-rj',
+      priority: 'high',
+      show_in_foreground: true,
+      content_available: true,
+    },
+    data: {
+      title: 'data_title',
+      body: 'data_body',
+      extra: 'data_extra',
+    },
+  };
+
+  let headers = new Headers({
+    'Content-Type': 'application/json',
+    Authorization: 'key=' + FIREBASE_SERVER_KEY,
+  });
+
+  let response = await fetch('https://fcm.googleapis.com/fcm/send', {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(message),
+  });
+  response = await response.json();
+  console.log(response);
 }
 
 function App(): JSX.Element {
@@ -61,6 +91,20 @@ function App(): JSX.Element {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  if (Platform.OS == 'android') {
+    requestAndroidPermission();
+  }
+
+  useEffect(() => {
+    const setUpCloudMessaging = async () => {
+      requestUserPermission();
+
+      const token = await messaging().getToken();
+      console.log('token is ' + token);
+    };
+    setUpCloudMessaging();
+  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -71,25 +115,29 @@ function App(): JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        <Text
+          style={[
+            styles.titleStyle,
+            {color: isDarkMode ? Colors.lighter : Colors.darker},
+          ]}>
+          Cloud Messaging Test
+        </Text>
+        <View style={styles.buttonStyle}>
+          <Button
+            title="Send to iOS"
+            onPress={() => {
+              sendMessage(iOSFCM);
+            }}
+          />
+        </View>
+
+        <View style={styles.buttonStyle}>
+          <Button
+            title="Send to Android"
+            onPress={() => {
+              sendMessage(androidFCM);
+            }}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -97,21 +145,13 @@ function App(): JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  titleStyle: {
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 18,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  buttonStyle: {
+    margin: 16,
   },
 });
 
---
+++ cloud_messaging/ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Loyd Kim on 2023-03-28.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
